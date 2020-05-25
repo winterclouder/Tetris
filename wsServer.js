38a5c2abbc1ddea67c1033b6354d26bad85e29fc
udnrainwu <rain.wu@udngroup.com.tw>
2020-05-25 15:59:31 +0800

    fix-upload

--- wsServer.js
+++ wsServer.js
@@ -1,7 +1,8 @@
 var app = require('express')()
 var server = require('http').createServer(app)
 var io = require('socket.io')(server)
-
+var express = require('express')
+app.use(express.static('public'))
 var PORT = 3000
 var clientCount = 0
 var bindListener = function(socket, enent) {
